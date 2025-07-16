989a07d3a01bebfae571325d118de57a0466421a
Sheraz uddin mirza <sherazuddin76@gmail.com>
2025-07-16 13:20:08 +0530

    Connected to supa database

--- Stock_market/settings.py
+++ Stock_market/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,12 +74,12 @@ WSGI_APPLICATION = 'Stock_market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -139,4 +140,11 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 CRON_CLASSES = {
     "stocks.cron.StockUpdateCronJob",
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='postgresql://postgres:Mirza%40%23120@db.cqwoabxobdqitiskdykb.supabase.co:5432/postgres',
+        conn_max_age=600
+    )
 }
